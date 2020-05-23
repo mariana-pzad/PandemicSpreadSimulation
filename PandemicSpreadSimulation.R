161cbc1e568d88bbe7c249b4fcccde9b712ea2e1
RamsesMG <rmedinag@uanl.edu.mx>
2020-05-23 14:42:47 -0500

    Primer Cambio

---
+++ PandemicSpreadSimulation.R
@@ -0,0 +1,88 @@
+#Inicializar la matriz
+Tablero <- matrix()
+
+#Filas representan a cada individuo
+#Columna 1: Identificador del individuo (número:ID)
+#Columna 2: Posicion dentro del Tablero (fila)
+#Columna 3: Posicion dentro del Tablero (columna)
+#Columna 4: Estado del individuo [0:susceptible, 1:enfermo, 2:vacunado, 3:recuperado, 4:latente, 5:muerto]
+#Columna 5: Edad [años]
+#Columna 6: Tasa de contagio [probabilidad]
+#Columna 7: Tasa de letalidad [probabilidad]
+#Columna 8: Tasa de vacunación [probabilidad]
+#Columna 9: Periodo de latencia [días]
+#Columna 10: Periodo de enfermedad [días]
+#Columna 11: Radio de movilidad
+#Columna 12: Trabaja
+#Columna 13: Tipo de traslado
+#Columna 14: Tipo de trabajo
+#Columna 15: Tasa de recuperación [probabilidad]
+#Columna 16: Cuarentena [binaria]
+
+
+
+#####################################################
+FuncionGraficar <- function(matriz){
+
+    return()
+}
+# Autores:
+  #Maricela Alejandra Valero Fuentes
+  #Edwin Martin Romero Silva
+  #Erick Hinojosa Aguirre
+
+######################################################
+MedidasPrev <- function(matriz){
+
+    return(matriz)
+}  
+#Luis David Dávila Torres
+#Carlos Antonio Espinosa Bravo
+#Alan Gerardo Garza Muro
+
+###################################################
+PonerEnCuarentena <- function(m) {
+    
+    return(matriz);
+}
+# Autores:
+# Victor Francisco Carrizales Castor
+# Natalia ALejandra García Armijo
+# Fabián Gandarilla López
+
+###################################################
+Contagio <- function(matriz){
+    
+    return(matriz)
+}
+#Autores:
+#Araceli Montserrat Rodriguez Crespo
+#Ivan Rigoberto Ibarra Rodriguez
+#Fabiola Jasso Valadez
+#####################################################
+Vacunacion <- function(matriz){
+return(matriz)
+}
+# Autores:
+# Gerardo Perez Arriega
+# Mariana Perez Arredondo
+
+##########################################################
+
+RecuperacionMuerte <- function(matriz){
+ 
+return(matriz)
+}
+#Autores:
+#Enrique Martinez
+#Sandra Negrete
+
+#####################################################
+FuncionAsintomaticos <- function(matriz){
+  
+    return()
+}
+#Autores:
+  #Luis Alberto Guerrero Zuñiga
+  #Alan Fernando Mejía Aranda
+  #Jose Armando Jara Rodriguez 
